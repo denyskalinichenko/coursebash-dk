dc9ff2327bbf9130d731fb2a836d5ac82c8041f1
DK <denis.kalinichenko96@gmail.com>
2024-12-18 16:31:32 +0100

    add lab3

---
+++ lab3/config.sh
@@ -0,0 +1,4 @@
+export INSTALL_DIR='/usr/finbook'
+export COMPRESS_LEVEL=9
+export TIMEOUT=30
+
---
+++ lab3/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#lab3 
+
+#Wczytaj zmienne z pliku config.sh
+source ./config.sh
+
+echo "Installation catalog: $INSTALL_DIR"
+echo "Compress level: $COMPRESS_LEVEL"
+echo "Timeout: $TIMEOUT"
+
+exit 0
+
